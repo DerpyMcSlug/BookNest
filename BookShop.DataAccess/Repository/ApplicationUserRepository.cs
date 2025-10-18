413ffba51c8ef8c2354ffd9dc942f96065c10ea4
DerpyMcSlug <mahuy2005@gmail.com>
2025-10-18 14:37:49 +0700

    Add files via upload

---
+++ BookShop.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookNest.DataAccess.Data;
+using BookNest.DataAccess.Repository.IRepository;
+using BookNest.Models;
+
+namespace BookNest.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ BookShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookNest.Models;
+
+namespace BookNest.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}

12cb1064572a0596ad005c724d1d2d74a1cb0a18
DerpyMcSlug <mahuy2005@gmail.com>
2025-10-18 14:36:58 +0700

    Delete BookShop.DataAccess/Repository directory

--- BookShop.DataAccess/Repository/IRepository/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BookShop.Models;
-
-namespace BookShop.DataAccess.Repository.IRepository
-{
-    public interface IProductRepository : IRepository<Product>
-    {
-        void Update(Product product);
-    }
-}
--- BookShop.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BookShop.Models;
-
-namespace BookShop.DataAccess.Repository.IRepository
-{
-    public interface IShoppingCartRepository : IRepository<ShoppingCart>
-    {
-        void Update(ShoppingCart shoppingCart);
-    }
-}

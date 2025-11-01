b1be99e26433233018cfde0c85dd57a34b766b1c
DerpyMcSlug <mahuy2005@gmail.com>
2025-11-01 15:10:58 +0700

    Add files via upload

---
+++ BookNest.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookNest.DataAccess.Data;
+using BookNest.DataAccess.Repository.IRepository;
+using BookNest.Models;
+
+namespace BookNest.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company company)
+        {
+            _db.Companies.Update(company);
+        }
+    }
+}
---
+++ BookNest.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookNest.DataAccess.Data;
+using BookNest.DataAccess.Repository.IRepository;
+using BookNest.Models;
+
+namespace BookNest.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}

c55cd2455f0a72b68ff4d5fed49be56329bc2a2c
DerpyMcSlug <mahuy2005@gmail.com>
2025-11-01 15:13:38 +0700

    Add files via upload

---
+++ BookNest.Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookNest.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public BaseModel()
+        {
+            Created = DateTime.Now;
+            Updated = DateTime.Now;
+        }
+    }
+}

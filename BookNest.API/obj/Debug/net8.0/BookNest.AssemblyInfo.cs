b4cbf382b0d8f3fb893b966078f346cc7028cff8
DerpyMcSlug <mahuy2005@gmail.com>
2025-12-10 12:46:43 +0700

    Fixed

--- BookNest.API/obj/Debug/net8.0/BookNest.AssemblyInfo.cs
+++ BookNest.API/obj/Debug/net8.0/BookNest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf43ddda803d9879adb55f5a6f041add91193ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30b5935fda1d338efae8b36887c979faf733aaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
